669ae01aceb259ebd331c0e817b89d294839d49a
Sofia <sofia.montgomery@gmail.com>
2022-03-25 17:37:55 +0100

    added second test

--- src/test/java/RSPTest.java
+++ src/test/java/RSPTest.java
@@ -1,10 +1,23 @@
 import org.junit.jupiter.api.*;
 
 public class RSPTest {
+    @BeforeEach
+    public void beforeTheEach () {
+        System.out.println("the test starts");
+    }
+    @AfterEach
+    public void afterTheEach () {
+        System.out.println("the test ends");
+    }
     @Test
     @DisplayName("test 1")
     public void whenThereISTwoInputsNoExceptionIsThrown (){
         RSP.playRSP(1, 2);
         Assertions.assertTrue(true);
+    }
+    @Test
+    @DisplayName("test 2")
+    public void whenTheInputsAreEqualThenThereIsADraw () {
+        //Assertions.assertEquals(2, RSP.playRSP(1,1));
     }
 }
