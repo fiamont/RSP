ed1552b7c4e095a5255eea6f648a9630dbc53c71
Sofia <sofia.montgomery@gmail.com>
2022-04-01 09:49:23 +0200

    edit method order

--- src/main/java/RSP.java
+++ src/main/java/RSP.java
@@ -2,6 +2,39 @@ import java.util.Random;
 
 public class RSP {
 
+    public static String player () {
+        Random random = new Random();
+        int playerInput = random.nextInt(3)+1;
+        if(playerInput==1){
+            return "rock";
+        }else if(playerInput==2){
+            return "scissor";
+        }else if (playerInput==3){
+            return "paper";
+        }
+        else {
+            return "";
+        }
+    }
+
+    public static String gameOutcome (String player1, String player2) {
+
+        if (player1.equals("rock") && player2.equals("scissor") || player1.equals("scissor") && player2.equals("paper") || player1.equals("paper") && player2.equals("rock")) {
+            return "player 1 wins";
+        }
+        else if (player2.equals("rock") && player1.equals("scissor") || player2.equals("scissor") && player1.equals("paper") || player2.equals("paper") && player1.equals("rock")) {
+            return "player 2 wins";
+        }
+        else {
+            return "draw";
+        }
+    }
+
+    public static int playMoreChoice () {
+        Random random = new Random();
+        return random.nextInt(3) + 1;
+    }
+
     public static void playRSP(String outcome) {
         System.out.println("Lets play Rock, scissor, paper! \n");
         String player1 = player();
@@ -10,6 +43,19 @@ public class RSP {
         System.out.println("Player 2 chose: " + player2);
         outcome = gameOutcome(player1,player2);
         System.out.println(outcome);
+    }
+
+    public static int playRSPUntilWin () {
+        int count = 0;
+        while (true){
+            playRSP((gameOutcome(player(),player())));
+            count++;
+            System.out.println(count);
+            if ((gameOutcome(player(),player())).equals("player 1 wins")){
+                break;
+            }
+        }
+        return count;
     }
 
     public static void playMore () {
@@ -29,51 +75,6 @@ public class RSP {
                 break;
 
             }
-        }
-    }
-    public static int playRSPUntilWin () {
-        int count = 0;
-        while (true){
-            playRSP((gameOutcome(player(),player())));
-            count++;
-            System.out.println(count);
-            if ((gameOutcome(player(),player())).equals("player 1 wins")){
-                break;
-            }
-        }
-        return count;
-    }
-
-    public static int playMoreChoice () {
-        Random random = new Random();
-        return random.nextInt(3) + 1;
-    }
-
-    public static String gameOutcome (String player1, String player2) {
-
-        if (player1.equals("rock") && player2.equals("scissor") || player1.equals("scissor") && player2.equals("paper") || player1.equals("paper") && player2.equals("rock")) {
-            return "player 1 wins";
-        }
-        else if (player2.equals("rock") && player1.equals("scissor") || player2.equals("scissor") && player1.equals("paper") || player2.equals("paper") && player1.equals("rock")) {
-            return "player 2 wins";
-        }
-        else {
-            return "draw";
-        }
-    }
-
-    public static String player () {
-        Random random = new Random();
-        int playerInput = random.nextInt(3)+1;
-        if(playerInput==1){
-            return "rock";
-        }else if(playerInput==2){
-            return "scissor";
-        }else if (playerInput==3){
-            return "paper";
-        }
-        else {
-            return "";
         }
     }
 }
